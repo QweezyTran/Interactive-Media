1262f3c62fb23470f1121dea63aa6e1a3298d88a
QweezyTran <35347399+QweezyTran@users.noreply.github.com>
2018-02-21 14:04:11 -0800

    Add files via upload

---
+++ analog_brightness/analog_brightness.ino
@@ -0,0 +1,19 @@
+const int LED = 9; // the pin for the LED
+int val = 0; // variable used to store the value
+// coming from the sensor
+void setup() {
+pinMode(LED, OUTPUT); // LED is as an OUTPUT
+// Note: Analogue pins are
+// automatically set as inputs
+}
+void loop() {
+val = analogRead(0); // read the value from
+// the sensor
+if(val>200) {
+analogWrite(LED, val/100); 
+} else if(val>100) {
+  analogWrite(LED, val);
+} else if(val>50) {
+  analogWrite(LED,val*100);
+}
+}
